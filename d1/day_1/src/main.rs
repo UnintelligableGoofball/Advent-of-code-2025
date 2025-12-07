1a8dc226df95107dfc3de8ff51d51ff37ffdf6a3
Blue <unfixedsoup@proton.me>
2025-12-07 02:21:12 -0600

    converts input file to list of ints

--- d1/day_1/src/main.rs
+++ d1/day_1/src/main.rs
@@ -1,19 +1,46 @@
-use std::io::{prelude::*, BufReader};
-use std::fs;
-use std::path;
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+    cmp::Eq
+};
 
 fn main() {
-    let instructions = parse_file("../d1_input");
-    for instruction in instructions {
-        println!("{:?}", instruction);
+    let turns = parse_file("../d1_input");
+    for turn in turns {
+        println!("{:?}", turn);
     }
+}
+
+fn parse_file(path: impl AsRef<Path>) -> Vec<i32> {
+    let file = File::open(path).expect("couldn't open file");
+    let buf = BufReader::new(file);
+
+    let stringy_turns: Vec<String> = buf.lines()
+        .map(|l| l.expect("couldn't parse line"))
+        .collect();
+
+    let mut vec = Vec::new();
+
+    for turn in stringy_turns.iter() {
+        let polarity = if turn.chars().nth(0) == Some('L') {
+            -1
+        } else {
+            1
+        };
+
+        let intern: i32 = turn.trim().trim_matches(char::is_alphabetic).parse().expect("fuuuuuuuck");
+
+        vec.push(intern * polarity)
+    }
+
+    vec
     
 }
 
-fn parse_file(path: impl AsRef<path::Path>) -> Vec<String> {
-    let file = fs::File::open(path).expect("couldn't open file");
-    let buf = BufReader::new(file);
-    buf.lines()
-        .map(|l| l.expect("couldn't parse line"))
-        .collect()
+/*
+fn process_turns(initial: i32, turns: Vec<String>) -> i32 {
+    // modulo is x.rem_euclid(y)
+    
 }
+*/
