e5c5558d85738cd57272002443a572f7ce24d492
Blue <unfixedsoup@proton.me>
2025-12-06 23:43:54 -0600

    very basic addition script

--- d1/day_1/src/main.rs
+++ d1/day_1/src/main.rs
@@ -1,3 +1,26 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut start = String::new();
+    let mut turn = String::new();
+    
+    println!("Current value?");
+
+    io::stdin()
+        .read_line(&mut start)
+        .expect("input failure");
+
+    println!("turn ammount?");
+
+    io::stdin()
+        .read_line(&mut turn)
+        .expect("input failure");
+
+    let start: i32 = start.trim().parse().expect("nan dummy");
+    let turn: i32 = turn.trim().parse().expect("nan dummy");
+
+    let result = &start + &turn;
+
+    println!("end is: {result}")
+    
 }
