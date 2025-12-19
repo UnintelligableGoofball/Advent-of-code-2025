37750e4579a2b1bdf54ce3de7e18035034644f87
UnintelligableGoofball <unfixedsoup@proton.me>
2025-12-19 14:02:54 -0600

    moreish?

--- d2/star1/src/main.rs
+++ d2/star1/src/main.rs
@@ -5,24 +5,33 @@ use std::{
 };
 
 fn main() {
-    let rangs = parse_line("../d2_input");
+    let input = parse_line("../d2_input");
+    println!("{:?}", input);
+    let yay = process_ranges(input);
 }
 
-fn parse_line(path: impl AsRef<Path>) {
+fn parse_line(path: impl AsRef<Path>) -> Vec<String> {
     let file = File::open(path).expect("no fiilleeeeeeeeeeeeee wahhhhhh");
     let mut buf = BufReader::new(file);
 
     let mut ranges_line = String::new();
     buf.read_line(&mut ranges_line).expect("naur");
     ranges_line = ranges_line.trim().to_string();
-    println!("{:?}", ranges_line);
 
-    let ranges_iter = ranges_line.split(",");
+    let rangs: Vec<String> = ranges_line.split(",").map(|s| s.to_string()).collect();
+    
+    rangs
+    
+}
 
-    let rangs_vec: Vec<String> = ranges_iter.to_string().collect();
-    
-    
+fn process_ranges(ranges: Vec<String>) -> i32 {
+    for range in ranges {
+        let ends: Vec<String> = range.split("-").map(|s| s.to_string()).collect();
+        for i in ends[1].parse::<i32>().unwrap()..ends[2].parse::<i32>().unwrap() {
+            if (i / 2) == i.to_string().split_at(i.to_string().len()).0.parse::<i32>().unwrap() {
+                println!("yay");
+            }
+        }
     }
-
-
-
+    1
+}
